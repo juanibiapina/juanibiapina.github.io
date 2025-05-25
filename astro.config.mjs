4aa9c6c8d2383fec72dadcab127773da38dc4ffa
Juan Ibiapina <juanibiapina@gmail.com>
2025-05-25 17:19:15 +0200

    chore: Prepare for tailwind 4 migration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juanibiapina.dev',
-  integrations: [tailwind()],
+  integrations: [],
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
