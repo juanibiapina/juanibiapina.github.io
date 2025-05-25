4c7b8e6a5927a3656d701f140c29d7f842c00438
Juan Ibiapina <juanibiapina@gmail.com>
2025-05-25 17:24:35 +0200

    dev: Update astro and tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,22 @@
 import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juanibiapina.dev',
   integrations: [],
+
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
+
   content: {
     remarkPlugins: [remarkReadingTime],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
